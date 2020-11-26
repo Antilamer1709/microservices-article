085acb5fc6a84c84f65eedac388b6d517d3cb232
vslobo <antilamer1709@gmail.com>
2020-11-26 17:54:11 +0200

    refactoring, service rollback

--- orders-application/orders/service/src/main/java/com/antilamer/orders/service/OrdersServiceImpl.java
+++ orders-application/orders/service/src/main/java/com/antilamer/orders/service/OrdersServiceImpl.java
@@ -1,5 +1,6 @@
 package com.antilamer.orders.service;
 
+import com.antilamer.inventory.service.InventoryService;
 import com.antilamer.orders.domain.OrderEntity;
 import com.antilamer.orders.dto.OrderDTO;
 import com.antilamer.orders.repository.OrderRepository;
@@ -12,8 +13,8 @@ public class OrdersServiceImpl implements OrdersService {
     @Autowired
     private OrderRepository orderRepository;
 
-//    @Autowired
-//    private InventoryService inventoryService;
+    @Autowired
+    private InventoryService inventoryService;
 
     @Override
     public OrderDTO getOrder(Integer orderId) {
@@ -23,10 +24,10 @@ public class OrdersServiceImpl implements OrdersService {
 
     @Override
     public String createOrder(OrderDTO orderDTO) {
-//        Boolean productAvailable = inventoryService.isProductAvailable(orderDTO.getProductId());
-//        if (!productAvailable) {
-//            return "Product with id " + orderDTO.getProductId() + " is not available";
-//        }
+        Boolean productAvailable = inventoryService.isProductAvailable(orderDTO.getProductId());
+        if (!productAvailable) {
+            return "Product with id " + orderDTO.getProductId() + " is not available";
+        }
         orderRepository.save(new OrderEntity(orderDTO.getProductId(), orderDTO.getName()));
         return "Order of product with id " + orderDTO.getProductId() + " has been created!";
     }
